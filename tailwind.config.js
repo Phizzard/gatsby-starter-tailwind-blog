47f741c997d27a71167462f8101db9330da48392
Phil Tietjen <tietjen.philip@gmail.com>
2019-10-09 07:55:14 -0400

    chore: rename tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      height: {
+        'screen-95': '95vh'
+      },
+      bg:{
+        primary: '#4299e1'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
